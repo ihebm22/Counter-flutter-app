49eba68a8682d46c4f670555fd7061a880f6e44f
ihebm22 <hebalamoudi20@gmail.com>
2025-04-03 10:28:53 +0300

    added flowers and a reset button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +20,7 @@ class MainApp extends StatelessWidget {
               end: Alignment.bottomCenter,
               colors: [
                 Color.fromARGB(255, 231, 247, 242),
-                Color.fromARGB(255, 186, 233, 218),
+                Color(0xFFBFEFFF),
               ],
             ),
           ),
@@ -41,11 +43,21 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   int counter = 0;
-String tree= "tree.png";
+  String tree = "tree.png";
+  bool showLeaves = false;
+  List<Offset> leaves = [];
 
   void increaseCounter() {
     setState(() {
-      counter++;
+      counter ++;
+      if (counter > 100) {
+        showLeaves = true;
+        if (counter % 5 == 0) {
+          double x = Random().nextDouble() * 200;
+          double y = Random().nextDouble() * 200;
+          leaves.add(Offset(x, y));
+        }
+      }
     });
   }
 
@@ -56,6 +68,10 @@ String tree= "tree.png";
       } else {
         counter = 0;
       }
+      if (counter >= 100 && counter%5==0 && leaves.isNotEmpty) {
+        leaves.removeLast();
+      }
+      
     });
   }
 
@@ -73,14 +89,13 @@ String tree= "tree.png";
                         color: Colors.black87,
                         fontSize: 15)),
               ),
-
               Padding(
                 padding: EdgeInsets.only(top: 10),
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.white,
                     shape: CircleBorder(),
-                  ), 
+                  ),
                   onPressed: () {
                     setState(() {
                       tree = "tree.png";
@@ -100,7 +115,7 @@ String tree= "tree.png";
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.white,
                     shape: CircleBorder(),
-                  ), 
+                  ),
                   onPressed: () {
                     setState(() {
                       tree = "palm.png";
@@ -117,7 +132,6 @@ String tree= "tree.png";
             ],
           ),
           SizedBox(height: 50),
-          
           Padding(
             padding: const EdgeInsets.only(top: 30, bottom: 50),
             child: Center(
@@ -131,61 +145,112 @@ String tree= "tree.png";
               ),
             ),
           ),
-          Container(
-            height: counter <=100 ? (50.0 + (counter * 3)).clamp(370, 600) : 370,
-            padding: EdgeInsets.only(bottom: 20),
-            alignment: Alignment.bottomCenter,
-            child: Image.asset(
-              'lib/assets/$tree',
-              height: 50.0 + (counter * 3),
-              fit: BoxFit.contain,
-            ),
-          ),
-          Row(
+          Stack(
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 100, right: 28),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    backgroundColor: Color.fromARGB(255, 255, 255, 255),
-                  ),
-                  onPressed: () {
-                    decreaseCounter();
-                    print("New counter: $counter");
-                  },
-                  child: Text("-",
-                      style: TextStyle(
-                          fontSize: 60,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(132, 0, 0, 0))),
+              Container(
+                height: counter <= 100
+                    ? (50.0 + (counter * 3)).clamp(370, 600)
+                    : 370,
+                padding: EdgeInsets.only(bottom: 20),
+                alignment: Alignment.bottomCenter,
+                child: Image.asset(
+                  'lib/assets/$tree',
+                  height: 50.0 + (counter * 3),
+                  fit: BoxFit.contain,
                 ),
               ),
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: CircleBorder(),
-                    padding: EdgeInsets.all(40),
-                    backgroundColor: Color.fromARGB(255, 124, 234, 190),
-                  ),
-                  onPressed: () {
-                    increaseCounter();
-                    print("Counter: $counter");
-                  },
-                  child: Text("Press me",
-                      style: TextStyle(
-                          fontStyle: FontStyle.italic,
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(243, 0, 0, 0))),
+              for (var leaf in leaves)
+                Positioned(
+                  right: leaf.dx + 160,
+                  top: leaf.dy + 55,
+                  child: tree == "tree.png"
+                      ? Image.asset(
+                          'lib/assets/flower.png',
+                          height: 30,
+                          width: 30,
+                          fit: BoxFit.contain,
+                        )
+                      : Image.asset(
+                          'lib/assets/coconut.png',
+                          height: 30,
+                          width: 30,
+                          fit: BoxFit.contain,
+                        ),
                 ),
-              ),
             ],
           ),
-          
-          
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 100, right: 28),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    onPressed: () {
+                      decreaseCounter();
+                      print("New counter: $counter");
+                    },
+                    child: Text("-",
+                        style: TextStyle(
+                            fontSize: 60,
+                            fontWeight: FontWeight.bold,
+                            color: const Color.fromARGB(132, 0, 0, 0))),
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(40),
+                      backgroundColor: Color(0xFF4CAF50),
+                    ),
+                    onPressed: () {
+                      increaseCounter();
+                      print("Counter: $counter");
+                    },
+                    child: Text("PRESS ME",
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                            //fontStyle: FontStyle.italic,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: const Color.fromARGB(243, 0, 0, 0))),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          counter = 0;
+                          showLeaves = false;
+                          leaves.clear();
+                        });
+                      },
+                      child: Text("Reset",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: const Color.fromARGB(243, 0, 0, 0))),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
